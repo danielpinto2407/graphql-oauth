9ac327ecc6b5b2b93d521c7affb0a7c02bc12780
Daniel <wdpinto@utp.edu.co>
2025-11-26 23:49:31 -0500

    (feature): proyexto inicializado solo con dependencias

---
+++ src/main/java/com/graphql_oauth/graphql_oauth/GraphqlOauthApplication.java
@@ -0,0 +1,13 @@
+package com.graphql_oauth.graphql_oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlOauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlOauthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/graphql_oauth/graphql_oauth/GraphqlOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.graphql_oauth.graphql_oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
